3ecf8f874663cb31d1772b220f3cb9932112a2ac
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-10-14 01:44:03 -0400

    Setting up CI/CD for google pages.

--- next.config.js
+++ next.config.js
@@ -2,13 +2,21 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      /* config for next-optimized-images */
-    },
-  ],
+const nextConfig = {
+  basePath: "portfolio",
+  assetPrefix: "portfolio",
+};
 
-  // your other plugins here
-]);
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        /* config for next-optimized-images */
+      },
+    ],
+
+    // your other plugins here
+  ],
+  nextConfig
+);
