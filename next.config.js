9ecf3c0236d4b2e138c85e4c665e0a90e661d4c3
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-11-05 23:49:04 -0400

    testing deploy settings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
+const deployEnv = process.env.deploy_env === "production";
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 const withMDX = require("@next/mdx")();
 
 const nextConfig = {
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio",
+  basePath: deployEnv ? "/portfolio" : "",
+  assetPrefix: deployEnv ? "/portfolio" : "",
 };
 
 module.exports = withPlugins(
