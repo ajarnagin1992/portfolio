1bf7575ee6f7ea6d50db997a5a286b565c2237e1
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-11-05 20:05:05 -0400

    reverting changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-// next.config.js
-const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 const withMDX = require("@next/mdx")();
 
 const nextConfig = {
-  basePath: ghPages ? "/portfolio/" : "",
-  assetPrefix: ghPages ? "/portfolio/" : "",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
 };
 
 module.exports = withPlugins(
