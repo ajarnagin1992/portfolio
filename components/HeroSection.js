dd597e6a1e77d2ae24b1ec8571e75dc8b69b89b8
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-11-08 17:23:20 -0500

    it says stuff

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -4,7 +4,7 @@ export const HeroSection = () => {
   return (
     <div className="flex flex-row justify-center items-center overflow-hidden">
       <div className="w-full md:w-1/2 text-center md:text-left p-20 space-y-4">
-        <p className="text-4xl">Hi, I'm Andrew!</p>
+        <p className="text-4xl">{"Hi, I'm Andrew!"}</p>
         <p className="text-xl">Full Stack Web-Developer / Back End Developer</p>
       </div>
       <div className="hidden lg:block relative w-full md:w-1/2 p-20">
