0cfddba05e1a02176a17028d3981d48aed0a6bf0
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-11-11 14:50:18 -0500

    Update ContactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,7 +9,7 @@ function ContactForm() {
       <div className="grid grid-cols-1 md:grid-cols-2 max-w-6xl m-auto rounded-b bg-blue-900 text-white p-20">
         <div>
           <h1 className="font-semibold text-xl">Reach out, lets chat!</h1>
-          <h2 classname="font-extralight">
+          <h2 className="font-extralight">
             {"Fill out the contact form and I'll get back to you ASAP!"}
           </h2>
 
@@ -63,7 +63,7 @@ function ContactForm() {
           className="flex flex-col max-w-6xl m-auto space-y-5 bg-white text-black p-10 rounded-xl"
           action="https://formspree.io/f/xleayqwr"
           method="POST"
-          enctype="multipart/form-data"
+          encType="multipart/form-data"
         >
           <label htmlFor="_replyto">Your email:</label>
           <input type="email" name="_replyto" className="rounded border p-2" />
