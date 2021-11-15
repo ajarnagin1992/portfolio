9eab347c84f49d271b4911e51eb43d007627609b
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-11-15 14:35:55 -0500

    test

--- components/SkillSection/SkillSection.js
+++ components/SkillSection/SkillSection.js
@@ -1,25 +1,12 @@
 import React from "react";
 import SkillCard from "./SkillCard";
-
-const skills = [
-  "Javascript",
-  "React",
-  "Angular",
-  "C#",
-  "Python",
-  "Elm",
-  "Haskell",
-  "Azure",
-  "Apache",
-  "MySQL",
-  "Node",
-];
+import { skills } from "./skills";
 
 function SkillSection() {
   return (
     <div className="grid grid-cols-4 grid-flow-row">
       {skills.map((skill) => {
-        return <SkillCard key={skill} icon="" skillName={skill} />;
+        return <SkillCard key={skill.key} icon="" skillName={skill.title} />;
       })}
     </div>
   );
---
+++ components/SkillSection/skills.js
@@ -0,0 +1,46 @@
+export const skills = [
+  {
+    title: "Javascript",
+    key: "javascript",
+  },
+  {
+    title: "React",
+    key: "react",
+  },
+  {
+    title: "Angular",
+    key: "angular",
+  },
+  {
+    title: "C#",
+    key: "c#",
+  },
+  {
+    title: "Python",
+    key: "python",
+  },
+  {
+    title: "Elm",
+    key: "elm",
+  },
+  {
+    title: "Haskell",
+    key: "haskell",
+  },
+  {
+    title: "Azure",
+    key: "azure",
+  },
+  {
+    title: "Apache",
+    key: "apache",
+  },
+  {
+    title: "MySQL",
+    key: "mySQL",
+  },
+  {
+    title: "Node",
+    key: "node",
+  },
+];
