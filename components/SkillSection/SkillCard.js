10ef331c8d727ce05ceb9fc264d02a5e474ef497
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-11-15 14:40:43 -0500

    test

--- components/SkillSection/SkillCard.js
+++ components/SkillSection/SkillCard.js
@@ -3,8 +3,8 @@ import React from "react";
 function SkillCard({ key, icon, skillName }) {
   return (
     <div key={key} className="p-4 flex flex-col">
-      <div key={key}>{icon}</div>
-      <div key={key}>{skillName}</div>
+      <div>{icon}</div>
+      <div>{skillName}</div>
     </div>
   );
 }
