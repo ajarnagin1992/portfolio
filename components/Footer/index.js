f3fb553c0177f94720a1f9d3b8c3457762983c53
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-11-17 08:52:19 -0500

    huge content fill

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
-    <div className="flex flex-col bg-white">
-      <div className="h-0.5 w-full bg-gray-600"></div>
-      <div className="flex flex-row space-x-10 p-5 justify-between font-light text-sm md:text-base  px-10 md:px-40">
+    <div className="flex flex-col">
+      <div className="h-0.5 w-full bg-gray-600 mt-10"></div>
+      <div className="flex flex-row space-x-10 p-5 justify-between font-light text-sm md:text-base  px-10 md:px-40 bg-white">
         <p>{"© 2021 Andrew Jarnagin. All rights reserved."}</p>
         <p>
           {"Built with "}
--- components/LatestCommits/index.js
+++ components/LatestCommits/index.js
@@ -21,16 +21,20 @@ function Profile() {
   if (!data) return <div>loading...</div>;
   else {
     return (
-      <div className="grid grid-cols-2 md:grid-cols-3">
-        {data.map((repo) => {
-          return (
-            <RepoCard
-              key={repo.id}
-              name={repo.name}
-              desc={repo.description}
-            ></RepoCard>
-          );
-        })}
+      <div>
+        <h1 className="text-5xl p-10">Latest Projects</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          {data.map((repo) => {
+            return (
+              <RepoCard
+                key={repo.id}
+                name={repo.name}
+                desc={repo.description}
+                url={repo.html_url}
+              ></RepoCard>
+            );
+          })}
+        </div>
       </div>
     );
   }
@@ -38,8 +42,18 @@ function Profile() {
 
 function RepoCard(props) {
   return (
-    <div className="flex flex-col p-3">
-      <h1>{props.name}</h1>
+    <div className="flex flex-col p-3 border border-black bg-gray-100 m-1 rounded space-y-1">
+      <div className="flex flex-row justify-between items-center">
+        <h1 className="text-lg font-semibold underline">{props.name}</h1>
+        <a
+          href={props.url}
+          target="_blank"
+          rel="noreferrer"
+          className=" text-xs p-2 rounded text-white bg-gradient-to-bl from-blue-400 to-blue-600"
+        >
+          Repository
+        </a>
+      </div>
       <p>{props.desc}</p>
     </div>
   );
