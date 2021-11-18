29bb8f07a698f47477648928ab126634e3836586
Andrew Jarnagin <ajarnagin1992@gmail.com>
2021-11-18 16:44:58 -0500

    meta changes

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -20,12 +20,12 @@ export const Meta = ({ ...metadata }) => {
       <meta property="og:site_name" content={meta.site_name} />
       <meta property="og:description" content={meta.description} />
       <meta property="og:title" content={meta.title} />
-      <meta property="og:image" content={addBasePath(meta.image)} />
+      <meta property="og:image" content={meta.image} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@ajarnagin1992" />
       <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
-      <meta name="twitter:image:src" content={addBasePath(meta.image)} />
+      <meta name="twitter:image:src" content={meta.image} />
     </Head>
   );
 };
